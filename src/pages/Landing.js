b4b5fa57f20f72289752ad65cf43923a4c085b21
chaosmos <vitovich1987@gmail.com>
2022-08-18 23:04:40 +0300

    created base routes structure

---
+++ src/pages/Landing.js
@@ -0,0 +1,31 @@
+import { Logo } from '../components';
+import main     from '../assets/images/main.svg';
+import Wrapper  from '../assets/wrappers/LandingPage';
+import { Link } from 'react-router-dom';
+
+const Landing = (props) => {
+  return (
+      <Wrapper>
+        <nav>
+          <Logo/>
+        </nav>
+        <div className="container page">
+          {/* info */}
+          <div className="info">
+            <h1>
+              job <span>tracking</span> app
+            </h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, amet
+               aperiam architecto, asperiores ea fugiat, itaque laborum magni
+               nobis quae quia quis quisquam reprehenderit similique suscipit
+               tempora tempore temporibus unde!
+            </p>
+            <Link to="/register" className="btn btn-hero">Login/Register</Link>
+          </div>
+          <img src={main} alt="job hunt" className="img main-img"/>
+        </div>
+      </Wrapper>
+  )
+};
+
+export default Landing;
